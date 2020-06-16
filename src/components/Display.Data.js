99da4b18d39b86bbefd2b796c20f8484139e39eb
catwhitmer <catwhitmer@gmail.com>
2020-06-16 13:51:28 -0400

    Done.

---
+++ src/components/Display.Data.js
@@ -0,0 +1,25 @@
+import React from 'react';
+ 
+class DisplayData extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.formData.firstName}</h1>
+        <h1>{this.props.formData.lastName}</h1>
+      </div>
+    )
+  }
+}
+ 
+export default DisplayData
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ParentComponent from './components/ParentComponent';
+ 
+ReactDOM.render(
+  <div>
+    <ParentComponent/>
+  </div>,
+  document.getElementById('root')
+);
